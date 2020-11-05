31f7edf22c50170da043937846e7cbeb9380bc13
Alex Bradford <alex.bradford417@gmail.com>
2020-11-05 16:27:37 -0500

    set up app.js and npm

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use("/api", apis);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
